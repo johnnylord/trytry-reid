34e035603ba511ead9dc23507a207841341a204d
johnnylord <johnnylord7410@gmail.com>
2021-01-29 14:36:30 +0800

    Remove mnist dataset

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -2,12 +2,10 @@ import sys
 
 from .market1501 import *
 from .dukemtmcreid import *
-from .mnist import *
 
 _dataset_classes = {
     'market1501': Market1501,
     'dukemtmcreid': DukeMTMCreID,
-    'mnist': MNIST,
     }
 
 def get_dataset_cls(name):
